c88169ea6828e9f941be16e7374f9e3cef05afce
ThiagoFrederico <thiagofrederico3@gmail.com>
2025-12-31 06:34:49 -0300

    fix env

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
     bucket         = "tf-state-challenge-bucket"
-    key            = "lambda/${var.environment}/terraform.tfstate"
     region         = "us-east-2"
     encrypt        = true
   }
