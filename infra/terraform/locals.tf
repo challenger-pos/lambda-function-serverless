58eb9b56ea652d24a2f74455e4ca159e78e3f574
ThiagoFrederico <thiagofrederico3@gmail.com>
2026-02-20 10:27:06 -0300

    change env

--- infra/terraform/locals.tf
+++ infra/terraform/locals.tf
@@ -1,3 +1,7 @@
 locals {
-  rds_state_path = "rds/${var.environment}/terraform.tfstate"
+  rds_state_path = "v4/rds-os/${var.environment}/terraform.tfstate"
+}
+
+locals {
+  networking_state_path = "v4/networking/${var.environment}/terraform.tfstate"
 }
